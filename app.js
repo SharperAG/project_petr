a9408792caa247160faaa8afec13a914a2542339
SharperJS <sharper.ag@mail.ru>
2021-08-26 11:18:21 +0500

    first

---
+++ app.js
@@ -0,0 +1,33 @@
+
+/* const categories = ["toys","guns","airplanes","tanks"];
+
+
+for(let i in categories){
+console.log(`mickey.com/${categories[i]}`);
+}
+
+let str = "i want to buy some toys like ";
+
+console.log(` ${str} ${categories[3]} and ${categories[1]}`); */
+
+let x = 10,
+    y = 20,
+    c = 30;
+
+/* const result = x + y === 30;
+
+console.log(`result is ${result}`); */
+
+/* if( (3*x*y) >= 4000){
+    console.log(`x  and y value is ${3*x*y} more than integer`);
+} else{
+    console.log(` ${3*x*y} less than integer `);
+}
+ */
+
+let decrem = 20;
+let increm = 39;
+
+decrem++;
+
+console.log(`decrem is ${decrem || increm}  `);
